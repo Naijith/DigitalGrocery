3e7380ec3d035b6d36694ce1d0442a797d0c6b74
Naijith <gnaijith78@gmail.com>
2020-08-21 00:33:39 +0530

    version 5

--- app.js
+++ app.js
@@ -1,64 +1,67 @@
 var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var fs = require('fs');
-var formidable = require('formidable')
-
+var formidable = require('formidable');
 var mysql = require('mysql2/promise');
+var path = require('path');
+var createError = require('http-errors');
+const { createSecretKey } = require("crypto");
 
-var new_con = mysql.createPool({
-	host: "192.168.64.2",
-    user: "naijith",
-    password: "naijith",
-	database: "digitalGrocery"
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+});
+const PORT = process.env.PORT || 3000;
+var new_con = mysql.createPool({
+  host: "192.168.64.2",
+  user: "naijith",
+  password: "naijith",
+  database: "digitalGrocery"
+  });
+var lists = [];
 
-
+app.set("view engine" , "ejs");
 app.use(express.static('./images'));
 app.use(express.static('./css'));
-app.set("view engine" , "ejs");
 app.use(bodyparser.urlencoded({extended : true}));
-
-var groceryList = []
-app.get("/groceryList",(req,res)=>{
-    console.log("printing list");
-    res.render("groceryList.ejs", {groceryList:groceryList});
-});
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/",(req,res)=>{
-
-    new_con.query("SELECT * FROM items", function(err,result,fields){
-        if (err){
-          console.log(err)
-          return
-        }
-        res.render("customer.ejs" , {items:result});
-    })
-    console.log("customer"); 
-})
+  new_con.query("SELECT * FROM items", function(err,result,fields){
+      if (err){
+        console.log(err)
+        return
+      }
+      res.render("customer.ejs" , {items:result});
+  })
+  console.log("customer"); 
+});
 
 app.get("/admin",(req,res)=>{
-
-    new_con.query("SELECT * FROM items", function(err,result,fields){
-        if (err){
-          console.log(err)
-          return
-        }
-        res.render("admin.ejs" , {items:result});
-    })
-    console.log("admin"); 
+  new_con.query("SELECT * FROM items", function(err,result,fields){
+    if (err){
+      console.log(err)
+      return
+    }
+    res.render("admin.ejs" , {items:result});
+  })
+  console.log("admin"); 
 })
 
 app.post("/add_item" , (req,res)=>{
+  var form = new formidable.IncomingForm();
 
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-      if (err) {
-          console.log(err);
-        }
-    
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+        console.log(err);
+    }
+  
     var name = fields.item_name;
     var imageType = "."+files.item_image.name.split(".").pop();
     var price = fields.item_price;
@@ -70,15 +73,15 @@ app.post("/add_item" , (req,res)=>{
     fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         console.log("success");
-      });
+    });
 
     new_con.query("INSERT INTO items (id,name,imageType,price,quantityType) VALUES (?,?,?,?,?)",[null,name,imageType,price,quantityType], function(err,result){
         if (err){
           console.log(err);
-        }
+        } 
     })
-    res.redirect("/admin"); 
-    });   
+  res.redirect("/admin"); 
+  });   
 });
 
 app.get("/delete_item/:name/:imageType" ,(req,res)=>{
@@ -99,12 +102,23 @@ app.get("/delete_item/:name/:imageType" ,(req,res)=>{
   res.redirect("/admin");
 });
 
-app.post("/submit_list" , (req,res)=>{
-    var name=req.body.item_name;
-
-    res.redirect("/")
+app.post("/new_order" , (req,res)=>{
+  lists.push(req.body.list);
+  console.log("new order submitted");
 });
 
-app.listen("2020",()=>{
-    console.log("server hosted......")
+app.get("/lists",(req,res)=>{
+  res.render("lists.ejs",{lists:lists});
 });
+
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+// app.use(function(err, req, res, next) {
+//   res.json(err.message);
+//   res.render('error.ejs');
+// });
+
+http.listen(PORT,() => console.log(`Server up on port ${PORT}`));
+
+module.exports=app;
